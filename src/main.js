03de21825604c61e7b0425df4add50b0143111c1
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-07-30 16:13:07 +0900

    a

--- src/main.js
+++ src/main.js
@@ -11,15 +11,16 @@ Vue.prototype.$axios = axios
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 
+// Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCQHORl8ANL2DIGa3vaW28On-J3mMIqU-o",
-  authDomain: "tenniscamp-782ef.firebaseapp.com",
-  databaseURL: "https://tenniscamp-782ef.firebaseio.com",
-  projectId: "tenniscamp-782ef",
-  storageBucket: "tenniscamp-782ef.appspot.com",
-  messagingSenderId: "116093815715",
-  appId: "1:116093815715:web:19d536d7d1cd702308e816",
-  measurementId: "G-DSHGC97SFY"
+  apiKey: "AIzaSyBDhU51b-WkErPNzaXRPDskIG6-obDjOyI",
+  authDomain: "tenniscamp-8eea9.firebaseapp.com",
+  databaseURL: "https://tenniscamp-8eea9.firebaseio.com",
+  projectId: "tenniscamp-8eea9",
+  storageBucket: "tenniscamp-8eea9.appspot.com",
+  messagingSenderId: "168459592873",
+  appId: "1:168459592873:web:32918be5d0d57fdbd735b8",
+  measurementId: "G-TPM96HKBQ9"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
